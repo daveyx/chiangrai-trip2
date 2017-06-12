f4c9b29deba8295e4f478c4e2d6737e07b19ab82
daveyx <d.rehle@gmail.com>
2017-06-12 05:58:35 +0200

    initial commit

---
+++ src/main/java/de/bluexs/crtrip/CrtripApplication.java
@@ -0,0 +1,12 @@
+package de.bluexs.crtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrtripApplication.class, args);
+	}
+}
