4186dd0e6f00e8de9cd7e4101265591e380e6735
daveyx <d.rehle@gmail.com>
2017-06-13 10:19:19 +0200

    update to latest libs

--- src/main/frontend/webpack.config.js
+++ src/main/frontend/webpack.config.js
@@ -1,20 +1,28 @@
+var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
     entry: './dev/index.js',
     devtool: 'sourcemaps',
     cache: true,
-    debug: true,
     output: {
         path: __dirname,
         filename: '../resources/static/built/bundle.js'
     },
+    plugins: [
+    	new webpack.LoaderOptionsPlugin({
+	        debug: true
+	    }),
+        new webpack.DefinePlugin({
+          BASENAME: JSON.stringify("/")
+        })
+      ],
     module: {
         loaders: [
             {
                 test: path.join(__dirname, '.'),
                 exclude: /(node_modules)/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 query: {
                     cacheDirectory: true,
                     presets: ['es2015', 'react']
