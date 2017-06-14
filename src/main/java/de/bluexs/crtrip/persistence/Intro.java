df6ecd8cb4157e4d7cb9fc3b4343d9b62c5896f0
daveyx <d.rehle@gmail.com>
2017-06-14 04:12:48 +0200

    added image desc

--- src/main/java/de/bluexs/crtrip/persistence/Intro.java
+++ src/main/java/de/bluexs/crtrip/persistence/Intro.java
@@ -22,15 +22,17 @@ public class Intro {
 	private String title;
 	private List<String> texts;
 	private String image;
+	private String imageText;
 	private List<String> images;
 	
 	@SuppressWarnings("unused")
 	private Intro() {}
 
-	public Intro(final String title, final List<String> texts, final String image, final List<String> images) {
+	public Intro(final String title, final List<String> texts, final String image, final String imageText, final List<String> images) {
 		this.title = title;
 		this.texts = texts;
 		this.image = image;
+		this.imageText = imageText;
 		this.images = images;
 	}
 	
