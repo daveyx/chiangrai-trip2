16869d0c5200c9a39f5f54d4a09b2334616e51bb
daveyx <d.rehle@gmail.com>
2017-06-14 03:46:46 +0200

    added intro reference

--- src/main/java/de/bluexs/crtrip/persistence/Day.java
+++ src/main/java/de/bluexs/crtrip/persistence/Day.java
@@ -3,6 +3,7 @@ package de.bluexs.crtrip.persistence;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 import lombok.Data;
 
@@ -20,14 +21,17 @@ public class Day {
 	private String title;
 	private String subtitle;
 	private String image;
+	@OneToOne
+	private Intro intro;
 	
 	@SuppressWarnings("unused")
 	private Day() {}
 
-	public Day(final String title, final String subtitle, final String image) {
+	public Day(final String title, final String subtitle, final String image, final Intro intro) {
 		this.title = title;
 		this.subtitle = subtitle;
 		this.image = image;
+		this.intro = intro;
 	}
 
 }
