d27db51c4aa959a81d9770f81bc669392bad7d88
daveyx <d.rehle@gmail.com>
2017-06-14 04:25:21 +0200

    intro repo

---
+++ src/main/java/de/bluexs/crtrip/persistence/IntroRepository.java
@@ -0,0 +1,17 @@
+package de.bluexs.crtrip.persistence;
+
+import org.springframework.data.repository.Repository;
+
+/**
+ * 
+ * @author daveyx
+ * 
+ */
+
+public interface IntroRepository extends Repository<Intro, Long> {
+
+	Intro findById(final Long id);
+
+	Intro save(Day day);
+
+}
