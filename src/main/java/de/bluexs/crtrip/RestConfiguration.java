595341ea3e710037387f2e248bb8f38ddf9068d1
daveyx <d.rehle@gmail.com>
2017-06-16 04:55:46 +0200

    cors enabled

---
+++ src/main/java/de/bluexs/crtrip/RestConfiguration.java
@@ -0,0 +1,32 @@
+package de.bluexs.crtrip;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 
+ * @author daveyx
+ * 
+ */
+
+// based on https://stackoverflow.com/questions/31724994/spring-data-rest-and-cors
+
+@Configuration
+public class RestConfiguration {
+	
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true); // you USUALLY want this
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("GET");
+	    config.addAllowedMethod("PUT");
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
+	}
+}
