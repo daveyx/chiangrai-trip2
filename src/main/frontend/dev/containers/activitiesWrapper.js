fa3b5baa592ba1f9a808a6bcd9fba73384a9284b
daveyx <d.rehle@gmail.com>
2017-06-17 04:59:53 +0200

    call to server

--- src/main/frontend/dev/containers/activitiesWrapper.js
+++ src/main/frontend/dev/containers/activitiesWrapper.js
@@ -6,10 +6,33 @@ export default class ActivitiesWrapper extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      data: null
     };
   }
 
+  componentDidMount() {
+    this.getData();
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps.day !== this.props.day) {
+      this.getData();
+    }
+  }
+
+  getData() {
+    axios.get('http://localhost:8080/api/days/' + (Number(this.props.day) + 1) + '/activities')
+      .then(response => {
+        this.setState({
+          data: response.data
+        });
+    }).catch(function (error) {
+      console.log('error axios-get activities: ' + error);
+    });
+  }
+
   render() {
+    console.log(this.state.data)
     return(
       <div>
         activities for day {this.props.day}
