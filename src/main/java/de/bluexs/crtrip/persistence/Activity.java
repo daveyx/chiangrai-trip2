6eb655fc415a0d869ae94b7fd5a4956002120033
daveyx <d.rehle@gmail.com>
2017-06-17 02:32:48 +0200

    activity entity

---
+++ src/main/java/de/bluexs/crtrip/persistence/Activity.java
@@ -0,0 +1,43 @@
+package de.bluexs.crtrip.persistence;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author daveyx
+ * 
+ */
+
+@Data
+@Entity
+@JsonInclude(Include.NON_NULL)
+public class Activity {
+
+	private @Id @GeneratedValue Long id;
+	private String title;
+	private String subtitle;
+	@ElementCollection(targetClass=String.class)
+	@Column(length=10000)
+	private List<String> texts;
+	
+	@SuppressWarnings("unused")
+	private Activity() {}
+
+	public Activity(final String title, final String subtitle, final List<String> texts) {
+		this.title = title;
+		this.subtitle = subtitle;
+		this.texts = texts;
+	}
+
+}
