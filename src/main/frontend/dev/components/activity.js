ed065f9d1a79d9aa137be8c400a6ea6204364be1
daveyx <d.rehle@gmail.com>
2017-06-18 08:53:03 +0200

    new google maps impl

--- src/main/frontend/dev/components/activity.js
+++ src/main/frontend/dev/components/activity.js
@@ -6,6 +6,7 @@ import {Grid, Row, Col} from 'react-bootstrap';
 import GMap from './googleMaps';
 // import Gallery from './gallery.jsx'
 import '../css/activity.css';
+import '../css/gmap.css';
 
 export default class Activity extends Component {
   constructor(props) {
@@ -27,21 +28,37 @@ export default class Activity extends Component {
       bgColor = 'bg-white';
     }
 
-    let content = ! this.props.activity ? null :
-      <Row>
-        <Col xs={12} md={6} mdPush={pullPushMd}>
-          {this.props.data.activity.position.coordinates.lng > 0 && this.props.data.activity.position.coordinates.lat > 0 ?
-          <div>
-            <h3>{this.props.data.activity.position.header}</h3>
-            <div className="map">
-              <GMap initialCenter={this.props.data.activity.position.coordinates} zoom={this.props.data.activity.position.zoom} />
-            </div>
-          </div>
-          : <p>Google Maps not available</p>
+    let settings = {
+      initialCenter: {
+        lat: Number(this.props.activity.gmap.lat),
+        lng: Number(this.props.activity.gmap.lng)
+      },
+      initialZoom: Number(this.props.activity.gmap.zoom),
+      markers: [
+        {
+          position: {
+            lat: Number(this.props.activity.gmap.lat),
+            lng: Number(this.props.activity.gmap.lng)
           }
-        </Col>
+        }
+      ],
+      snapToUserLocation: false
+    };
+
+    const gMap = this.props.activity.gmap.lng > 0 && this.props.activity.gmap.lat > 0 ?
+        <Col xs={12} md={6} mdPush={pullPushMd} className="maps-col">
+            <h3>{this.props.activity.gmap.title}</h3>
+            <GMap config={settings} id={this.props.activity.id} />
+        </Col> :
+        <Col xs={12} md={6} mdPush={pullPushMd}>
+          <p>Google Maps not available</p>
+        </Col>;
+
+    let content = ! this.props.activity ? null :
+      <Row className="is-table-row">
+        {gMap}
         <Col xs={12} md={6} mdPull={pullPushMd}>
-          <h3>{this.props.activity.activityHeader2}</h3>
+          <h3>{this.props.activity.subtitle}</h3>
           {this.props.activity.texts.map(function(data, index){
             return <p key={index}>{data}</p>;
           })}
