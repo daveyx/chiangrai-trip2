bdda8a70294d10b3f5c5ceb822a96ca2b545ccd5
daveyx <d.rehle@gmail.com>
2017-06-18 11:24:19 +0200

    general fixes

---
+++ src/main/frontend/dev/components/gallery.js
@@ -0,0 +1,24 @@
+import ImageGallery from 'react-image-gallery';
+import React from 'react';
+import '../css/gallery.css';
+
+export default class Gallery extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleImageLoad(event) {
+  }
+
+  render() {
+    return (
+      <ImageGallery
+        items={this.props.images}
+        slideInterval={2000}
+        showIndex={true}
+        infinite={false}
+        lazyLoad={false}
+        onImageLoad={this.handleImageLoad} />
+    );
+  }
+}
