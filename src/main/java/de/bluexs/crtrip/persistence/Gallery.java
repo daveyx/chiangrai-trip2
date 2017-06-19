5db3f263f42cd93ffc831f1bc90827b2b408320a
daveyx <d.rehle@gmail.com>
2017-06-19 02:09:14 +0200

    gallery entity

---
+++ src/main/java/de/bluexs/crtrip/persistence/Gallery.java
@@ -0,0 +1,40 @@
+package de.bluexs.crtrip.persistence;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * @author daveyx
+ * 
+ */
+
+@Data
+@Entity
+@NoArgsConstructor
+@JsonInclude(Include.NON_NULL)
+public class Gallery {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String title;
+	
+	@OneToMany(mappedBy = "activity")
+    private List<GalleryImage> gallery;
+	
+	public Gallery(final String title) {
+		this.title = title;
+	}
+}
