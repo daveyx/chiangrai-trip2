3f88641195c4be82a77e5d5ad38fd33162782cc3
daveyx <d.rehle@gmail.com>
2017-06-19 10:59:32 +0200

    removed frontend stuff

--- src/main/frontend/dev/config.js
+++
@@ -1,3 +0,0 @@
-export const config =  {
-    API_URL: 'https://www.chiangrai-trip.ga/api'
-};
--- src/main/frontend/dev/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import Routes from './routes.js';
-
-render(
-  <Routes />,
-  document.getElementById('react')
-)
