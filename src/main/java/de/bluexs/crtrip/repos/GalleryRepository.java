d51990d9b105fc39188da00f2752941a5fc26418
daveyx <d.rehle@gmail.com>
2017-06-19 02:11:40 +0200

    gallery repo

---
+++ src/main/java/de/bluexs/crtrip/repos/GalleryRepository.java
@@ -0,0 +1,21 @@
+package de.bluexs.crtrip.repos;
+
+import org.springframework.data.repository.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import de.bluexs.crtrip.persistence.Gallery;
+
+/**
+ * 
+ * @author daveyx
+ * 
+ */
+
+@RepositoryRestResource(exported = false)
+public interface GalleryRepository extends Repository<Gallery, Long>{
+
+	Gallery save(final Gallery image);
+
+	Gallery findById(final Long id);
+	
+}
