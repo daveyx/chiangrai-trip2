0de5c14a4e21e868bdf9a5ee7b0b8c00ca2b515f
daveyx <d.rehle@gmail.com>
2017-06-23 15:20:34 +0200

    moved to security package

--- src/main/java/de/bluexs/crtrip/security/SpringDataJpaUserDetailsService.java
+++ src/main/java/de/bluexs/crtrip/security/SpringDataJpaUserDetailsService.java
@@ -1,4 +1,4 @@
-package de.bluexs.crtrip;
+package de.bluexs.crtrip.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
