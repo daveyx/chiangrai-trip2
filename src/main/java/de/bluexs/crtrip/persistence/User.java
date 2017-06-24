3e8c1e4fd389fa71c6f9131096999f744b855c2e
daveyx <d.rehle@gmail.com>
2017-06-24 13:27:53 +0200

    explicit table name

--- src/main/java/de/bluexs/crtrip/persistence/User.java
+++ src/main/java/de/bluexs/crtrip/persistence/User.java
@@ -9,11 +9,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
+@Table(name = "users")
 public class User implements UserDetails {
 
 	static final long serialVersionUID = 1L;
