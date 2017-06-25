bf4c47860c39c91b0b1f2956affe4e938b3c1573
daveyx <d.rehle@gmail.com>
2017-06-25 03:35:34 +0200

    removed dead code

--- src/main/java/de/bluexs/crtrip/security/OAuth2Config.java
+++ src/main/java/de/bluexs/crtrip/security/OAuth2Config.java
@@ -43,13 +43,4 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 		clients.inMemory().withClient("daveyx").secret("secret").accessTokenValiditySeconds(expiration)
 				.scopes("read", "write").authorizedGrantTypes("password", "refresh_token").resourceIds("resource");
 	}
-
-	@Override
-	protected void configure(final HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/**").anonymous()
-	            .and()
-	            .csrf().disable();
-	}
 }
