60371d5624ebbf0768b0155c57ad3a494eb5deaa
daveyx <d.rehle@gmail.com>
2017-06-25 13:14:27 +0200

    code format

--- src/main/java/de/bluexs/crtrip/security/OAuth2Config.java
+++ src/main/java/de/bluexs/crtrip/security/OAuth2Config.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +12,12 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+/**
+ * 
+ * @author daveyx
+ * 
+ */
 
 @Configuration
 @EnableAuthorizationServer
@@ -43,4 +48,5 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 		clients.inMemory().withClient("daveyx").secret("secret").accessTokenValiditySeconds(expiration)
 				.scopes("read", "write").authorizedGrantTypes("password", "refresh_token").resourceIds("resource");
 	}
+
 }
