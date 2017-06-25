b6f61191d9e1c4edfa4e44adfba4383d96ba32e7
daveyx <d.rehle@gmail.com>
2017-06-25 14:03:37 +0200

    renamed

--- src/main/java/de/bluexs/crtrip/controllers/RegistrationController.java
+++ src/main/java/de/bluexs/crtrip/controllers/RegistrationController.java
@@ -13,10 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-public class GreetingController {
+public class RegistrationController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+
+//    @RequestMapping(value = "/user/registration", method = RequestMethod.POST)
+//    @ResponseBody
+//    public GenericResponse registerUserAccount(@Valid final UserDto accountDto, final HttpServletRequest request) {
+//        LOGGER.debug("Registering user account with information: {}", accountDto);
+//
+//        final User registered = userService.registerNewUserAccount(accountDto);
+//        eventPublisher.publishEvent(new OnRegistrationCompleteEvent(registered, request.getLocale(), getAppUrl(request)));
+//        return new GenericResponse("success");
+//    }
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") final String name) {
