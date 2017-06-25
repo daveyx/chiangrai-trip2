d828e926e988c9ad544c4ab16d1440f8e5f65296
daveyx <d.rehle@gmail.com>
2017-06-25 16:22:00 +0200

    fixed wrong password

--- src/main/java/de/bluexs/crtrip/persistence/UserDatabaseLoader.java
+++ src/main/java/de/bluexs/crtrip/persistence/UserDatabaseLoader.java
@@ -24,11 +24,9 @@ public class UserDatabaseLoader implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-		// $2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri
-		// $2a$10$f16d85rMfM/fLFCEet/66ePGmw2O4qu1HnYRxFLYD64Yi77gD48Xu
-		final User user1 = new User(null, "peter@example.com", "$2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri");
-		final User user2 = new User(null, "katie@example.com", "$2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri");
-		final User user3 = new User(null, "john@example.com", "$2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri");
+		final User user1 = new User(null, "peter@example.com", "password");
+		final User user2 = new User(null, "katie@example.com", "password");
+		final User user3 = new User(null, "john@example.com", "password");
 		
 		users.save(user1);
 		users.save(user2);
