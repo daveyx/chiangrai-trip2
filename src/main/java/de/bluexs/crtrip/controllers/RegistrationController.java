5d8f0792e2ffc100b4f2752381d48c363e99e20c
daveyx <d.rehle@gmail.com>
2017-06-25 16:28:07 +0200

    registrationcontroller

--- src/main/java/de/bluexs/crtrip/controllers/RegistrationController.java
+++ src/main/java/de/bluexs/crtrip/controllers/RegistrationController.java
@@ -1,10 +1,20 @@
 package de.bluexs.crtrip.controllers;
 
-import java.util.concurrent.atomic.AtomicLong;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import de.bluexs.crtrip.persistence.User;
+import de.bluexs.crtrip.service.IUserService;
+import de.bluexs.crtrip.util.GenericResponse;
+import de.bluexs.crtrip.util.UserDto;
 
 /**
  * 
@@ -14,24 +24,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegistrationController {
+	
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    @Autowired
+    private IUserService userService;
 
-//    @RequestMapping(value = "/user/registration", method = RequestMethod.POST)
-//    @ResponseBody
-//    public GenericResponse registerUserAccount(@Valid final UserDto accountDto, final HttpServletRequest request) {
-//        LOGGER.debug("Registering user account with information: {}", accountDto);
-//
-//        final User registered = userService.registerNewUserAccount(accountDto);
-//        eventPublisher.publishEvent(new OnRegistrationCompleteEvent(registered, request.getLocale(), getAppUrl(request)));
-//        return new GenericResponse("success");
-//    }
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    @ResponseBody
+    public GenericResponse registerUserAccount(@Valid final UserDto accountDto, final HttpServletRequest request) {
+        LOGGER.debug("Registering user account with information: {}", accountDto);
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") final String name) {
-        return new Greeting(counter.incrementAndGet(),
-        		String.format(template, name));        
+        @SuppressWarnings("unused")
+		final User registered = userService.registerNewUserAccount(accountDto);
+        return new GenericResponse("success");
     }
-}
 
+}
