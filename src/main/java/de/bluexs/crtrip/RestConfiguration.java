859f08e968b9491edb020dfc28f6bd5bcff4a614
daveyx <d.rehle@gmail.com>
2017-06-26 08:56:48 +0200

    enable cors and options requests on oauthserver

--- src/main/java/de/bluexs/crtrip/RestConfiguration.java
+++ src/main/java/de/bluexs/crtrip/RestConfiguration.java
@@ -1,7 +1,10 @@
 package de.bluexs.crtrip;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -15,19 +18,39 @@ import org.springframework.web.filter.CorsFilter;
 // based on https://stackoverflow.com/questions/31724994/spring-data-rest-and-cors
 
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class RestConfiguration {
 	
 	@Bean
 	public CorsFilter corsFilter() {
 	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    final CorsConfiguration config = new CorsConfiguration();
+	    final CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
 	    config.setAllowCredentials(true); // you USUALLY want this
 	    config.addAllowedOrigin("*");
 	    config.addAllowedHeader("*");
 	    config.addAllowedMethod("GET");
 	    config.addAllowedMethod("PUT");
 	    config.addAllowedMethod("POST");
+	    config.addAllowedMethod("OPTIONS");
 	    source.registerCorsConfiguration("/**", config);
 	    return new CorsFilter(source);
 	}
+	
+    @Bean
+    public FilterRegistrationBean corsFilterRegistration() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+	    config.addAllowedMethod("GET");
+	    config.addAllowedMethod("PUT");
+	    config.addAllowedMethod("POST");
+	    config.addAllowedMethod("OPTIONS");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
