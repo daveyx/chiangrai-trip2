b4db5e9afa026df95004f8311d1fd065637bda70
daveyx <d.rehle@gmail.com>
2017-06-26 03:54:48 +0200

    disabled greeting controller

--- src/main/java/de/bluexs/crtrip/play/GreetingController.java
+++ src/main/java/de/bluexs/crtrip/play/GreetingController.java
@@ -18,21 +18,21 @@ import de.bluexs.crtrip.util.GenericResponse;
  * 
  */
 
-@RestController
+//@RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
-
-    @RequestMapping(value = "/greeting", method = RequestMethod.POST)
-    @ResponseBody
-    public GenericResponse greetingPost(final HttpServletRequest request) {
-        return new GenericResponse("success");
-    }
+//    private static final String template = "Hello, %s!";
+//    private final AtomicLong counter = new AtomicLong();
+//
+//    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+//    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+//        return new Greeting(counter.incrementAndGet(),
+//                            String.format(template, name));
+//    }
+//
+//    @RequestMapping(value = "/greeting", method = RequestMethod.POST)
+//    @ResponseBody
+//    public GenericResponse greetingPost(final HttpServletRequest request) {
+//        return new GenericResponse("success");
+//    }
 }
