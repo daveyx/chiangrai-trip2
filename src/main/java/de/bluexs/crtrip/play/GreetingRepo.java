295ad64643cae9fa483b6d63e292c483a76d9528
daveyx <d.rehle@gmail.com>
2017-06-26 04:29:44 +0200

    code format

--- src/main/java/de/bluexs/crtrip/play/GreetingRepo.java
+++ src/main/java/de/bluexs/crtrip/play/GreetingRepo.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * 
  */
 
-public interface GreetingRepo extends JpaRepository<Greeting, Long>{
+public interface GreetingRepo extends JpaRepository<Greeting, Long> {
 
 }
