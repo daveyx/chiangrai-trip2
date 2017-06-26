5d3866ff82025497f290c982bdd02d04fbbda4fd
daveyx <d.rehle@gmail.com>
2017-06-26 03:40:53 +0200

    added a post mapping

--- src/main/java/de/bluexs/crtrip/play/GreetingController.java
+++ src/main/java/de/bluexs/crtrip/play/GreetingController.java
@@ -2,9 +2,15 @@ package de.bluexs.crtrip.play;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import de.bluexs.crtrip.util.GenericResponse;
 
 /**
  * 
@@ -18,9 +24,15 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.POST)
+    @ResponseBody
+    public GenericResponse greetingPost(final HttpServletRequest request) {
+        return new GenericResponse("success");
     }
 }
