6147187810aee4082cffcd65a5203c40952470ed
daveyx <d.rehle@gmail.com>
2017-06-26 03:41:13 +0200

    moved to an entity

--- src/main/java/de/bluexs/crtrip/play/Greeting.java
+++ src/main/java/de/bluexs/crtrip/play/Greeting.java
@@ -1,26 +1,26 @@
 package de.bluexs.crtrip.play;
 
+import javax.persistence.Entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * 
  * @author daveyx
  * 
  */
 
+@Data
+@Entity
+@NoArgsConstructor
 public class Greeting {
 
-    private final long id;
-    private final String content;
+    private long id;
+    private String content;
 
-    public Greeting(long id, String content) {
+    public Greeting(final long id, final String content) {
         this.id = id;
         this.content = content;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
     }
 }
