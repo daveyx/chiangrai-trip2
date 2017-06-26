dc0fb3049ce66c82640c0e8141a05206ef8c8e2a
daveyx <d.rehle@gmail.com>
2017-06-26 03:54:07 +0200

    added greeting repo

---
+++ src/main/java/de/bluexs/crtrip/play/GreetingRepo.java
@@ -0,0 +1,13 @@
+package de.bluexs.crtrip.play;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 
+ * @author daveyx
+ * 
+ */
+
+public interface GreetingRepo extends JpaRepository<Greeting, Long>{
+
+}
