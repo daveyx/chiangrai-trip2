44fad2ca81889a08906ee0b425ccbc4cc16f569d
daveyx <d.rehle@gmail.com>
2017-06-28 11:55:09 +0200

    entity for social user

---
+++ src/main/java/de/bluexs/crtrip/social/persistence/SocialUser.java
@@ -0,0 +1,48 @@
+package de.bluexs.crtrip.social.persistence;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import de.bluexs.crtrip.persistence.User;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"userId", "providerId", "providerUserId"}),
+		@UniqueConstraint(columnNames = {"userId", "providerId", "rank"})})
+public class SocialUser {
+
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER, optional = false)
+	@JoinColumn(name = "userId", nullable = false, updatable = false)
+    private User user;
+
+    private String providerId;
+
+    private String providerUserId;
+
+    private int rank;
+
+    private String displayName;
+
+    private String profileUrl;
+
+    private String imageUrl;
+
+    @Column(length = 500)
+    private String accessToken;
+
+    private String secret;
+
+    private String refreshToken;
+
+    private Long expireTime;
+    
+    @Column(length=36)
+    private String uuid;
+}
